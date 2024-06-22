01fbc0a36b56487aa7f97712615d4264e74526d0
Dragonpark <dragonpark@gmail.com>
2024-06-21 21:31:42 -0400

    debuggers

--- lib/smart_proxy_ipam/ipam_api.rb
+++ lib/smart_proxy_ipam/ipam_api.rb
@@ -47,6 +47,7 @@ module Proxy::Ipam
         validate_required_params!([:address, :prefix], params)
 
         mac_param = params[:mac]
+        logger.debug(params)
         mac = validate_mac!(params[:mac]) unless mac_param.nil? || mac_param.empty?
         cidr = validate_cidr!(params[:address], params[:prefix])
         group_name = get_request_group(params)
@@ -327,6 +328,7 @@ module Proxy::Ipam
 
         ip_added = provider.add_ip_to_subnet(ip, add_ip_params) # Returns nil on success
         halt 500, ip_added.to_json unless ip_added.nil?
+        logger.debug('Added IP #{ip} for host in IPAM')
         status 201
       rescue Proxy::Validations::Error => e
         logger.warn(e.message)
